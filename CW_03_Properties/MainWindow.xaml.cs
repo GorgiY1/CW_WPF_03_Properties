fae9a4ba672421174915f98c09b3885be4c5b91e
johnny <orgeorgij@gmail.com>
2024-12-31 12:23:08 +0200

    Add project files.

---
+++ CW_03_Properties/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CW_03_Properties
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                double size;
+                if (double.TryParse(button.Content.ToString()?.Substring
+                    (button.Content.ToString().LastIndexOf(' ') + 1),out size))
+                {
+                    FontSize = size;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button button)
+            {
+                double size;
+                if (double.TryParse(button.Name.ToString()?.Substring
+                    (button.Name.ToString().LastIndexOf('_') + 1), out size))
+                {
+                    button.FontSize = size;
+                }
+            }
+        }
+
+        private void SpaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is SpaceButton button)
+            {
+                button.Space = Convert.ToInt32(button.Tag);
+            }
+        }
+    }
+}
---
+++ CW_03_Properties/SpaceButton.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CW_03_Properties
+{
+    class SpaceButton : Button
+    {
+        public int Space
+        {
+            get { return (int)GetValue(SpaceProperty); }
+            set { SetValue(SpaceProperty, value); }
+        }
+
+        private string _text;
+        public string Text 
+        { 
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                Content = SpaceOutText(_text);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for Space. This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SpaceProperty;
+
+        static SpaceButton()
+        {
+            FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata
+            {
+                DefaultValue = 1,
+                AffectsMeasure = true,
+                Inherits = true
+            };
+            metadata.PropertyChangedCallback += OnSpaseProperyChanged;
+
+            SpaceProperty = DependencyProperty.Register("Space", typeof(int), typeof(SpaceButton),
+                metadata,ValidateSpaceValue);
+        }
+
+        
+        private static bool ValidateSpaceValue(object value)
+        {
+            return (int)value >= 0 && (int)value < 10;
+        }
+
+        private static void OnSpaseProperyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is SpaceButton spaceButton)
+            {
+                spaceButton.Content = spaceButton.SpaceOutText(spaceButton.Text);
+            }
+        }
+
+        private string SpaceOutText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return text;
+            }
+            StringBuilder builder = new StringBuilder();
+
+            foreach (char item in text)
+            {
+                builder.Append($"{item}{new string(' ', Space)}");
+            }
+            return builder.ToString();
+        }
+        //DependencyProperty.Register("Space", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
+
+
+
+
+    }
+}
